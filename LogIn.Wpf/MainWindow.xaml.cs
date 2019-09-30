9bc7daa8b1694e9bd8f45b76250a2b08fa25bd48
Stef Vercaemer <stefaan.vercaemer@howest.be>
2019-09-30 23:17:09 +0200

    Add files via upload

---
+++ LogIn.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace LogIn.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string USERNAME = "admin";
+        const string PASWOORD = "1234";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAanmelden_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
